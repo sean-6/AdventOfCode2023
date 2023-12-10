11b023c1ae386b6b58b4807ca27c1f161d94bb6b
Sean <seanc23lfc@gmail.com>
2023-12-10 10:01:26 +0000

    Solve Day 2 P2

--- Days/Day02.cs
+++ Days/Day02.cs
@@ -16,9 +16,9 @@
             {
                 var colours = new Dictionary<string, int>
                 {
-                    {"blue", 14 },
-                    {"red", 12 },
-                    {"green", 13 }
+                    { "blue", 14 },
+                    { "red", 12 },
+                    { "green", 13 }
                 };
 
                 var splitInTwo = line.Split(":");
@@ -31,7 +31,6 @@
 
                 var contentWithNumbers = games.Select(game => game.Trim().Split(" ")).ToList();
 
-
                 if (contentWithNumbers.All(x => int.Parse(x[0]) <= colours[x[1]]))
                     sum += id;
             }
@@ -41,7 +40,45 @@
 
         public override string Solve2()
         {
-            return string.Empty;
+            var sum = 0;
+            foreach (var line in _input)
+            {
+                var lowest = new Dictionary<string, int>
+                {
+                    { "blue", 0 },
+                    { "red", 0 },
+                    { "green", 0 }
+                };
+
+                var colours = new Dictionary<string, int>
+                {
+                    { "blue", 14 },
+                    { "red", 12 },
+                    { "green", 13 }
+                };
+
+                var splitInTwo = line.Split(":");
+                var id = int.Parse(splitInTwo[0].Split(" ")[1]);
+
+                var bagContents = splitInTwo[1].TrimStart();
+
+                bagContents = bagContents.Replace(",", ";");
+                var games = bagContents.Split(";");
+
+                var contentWithNumbers = games.Select(game => game.Trim().Split(" ")).ToList();
+
+                foreach (var item in contentWithNumbers)
+                {
+                    var count = int.Parse(item[0]);
+                    var colour = item[1];
+
+                    if (count > lowest[colour])
+                        lowest[colour] = count;
+                }
+
+                sum += lowest.Values.Aggregate((a, b) => a * b);
+            }
+            return sum.ToString();
         }
     }
 }
